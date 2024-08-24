9e7aa2f19b0c042d470f5905bb1908e64ac06614
notcl4y14 <notcl4y@gmail.com>
2024-08-24 15:25:42 +0300

    Add palette.lua

---
+++ tests/palette.lua
@@ -0,0 +1,35 @@
+local a = {}
+
+local colors = require("../common/colors")
+
+local tileWidth = 256 / 10
+local tileHeight = 256 / 10
+
+local function load()
+end
+
+local function update(dt)
+end
+
+local function draw()
+	local x = 0
+	local y = 0
+
+	for i = 0, 20 do
+		if i > 0 and i % 10 == 0 then
+			x = 0
+			y = y + 1
+		end
+
+		love.graphics.setColor(colors[i])
+		love.graphics.rectangle("fill", x * tileWidth, y * tileHeight, tileWidth, tileHeight)
+
+		x = x + 1
+	end
+end
+
+a.load = load
+a.update = update
+a.draw = draw
+
+return a
